93b0e4d75fd3668ba8ce308b0f9f836f545b60da
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-10-16 16:31:32 +0200

    initial commit for frpnow

--- src/Frp/Banana.hs
+++ src/Frp/Banana.hs
@@ -1,6 +1,7 @@
 module Frp.Banana where
 
 import           ClassyPrelude               hiding (union)
+{-
 import           Reactive.Banana.Combinators
 
 merge :: (Semigroup (f (Either t1 t2)),Functor f) => f t1 -> f t2 -> (t1 -> x) -> (t2 -> x) -> f x
@@ -8,4 +9,4 @@ merge e1 e2 f1 f2 = either f1 f2 <$> ((Left <$> e1) <> (Right <$> e2))
 
 instance Semigroup (Event t a) where
   (<>) = union
-
+-}
